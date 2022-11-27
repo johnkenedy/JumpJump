1b187502edf0defa915bf1036c5d864048489a82
john kenedy <john.kenedy232@gmail.com>
2022-11-27 12:17:01 -0300

    Initial commit

---
+++ app/src/main/java/io/project/jumpjump/MainActivity.kt
@@ -0,0 +1,17 @@
+package io.project.jumpjump
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.project.putushitogether.R
+import io.project.putushitogether.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
